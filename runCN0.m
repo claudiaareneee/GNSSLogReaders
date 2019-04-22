d61ddbd8d9393007dae9f0e92a0005114f57c1a6
Claudia Nelson <claudiaareneee@gmail.com>
2019-04-22 12:22:01 -0500

    simplifying files

--- runCN0.m
+++ runCN0.m
@@ -5,3 +5,35 @@ function runCN0(fileFolder, gnssFile)
   
     disp("end of plotting function");
 end
+
+function plotCN0(gnss,initial_time,sessionName,fileFolder)
+gnssMapKeys = cell2mat(keys(gnss));
+
+for i = 1:length(keys(gnss))
+    fig = configurePlot();
+    legend_strings = [];
+    
+    % Reading the measurements by satellite
+    currentMap = gnss(gnssMapKeys(i));
+    currentKeys = cell2mat(keys(currentMap));
+    
+    for j = 1:length(currentKeys)
+        signaltype = getInfo(gnssMapKeys(i),currentKeys(j));
+        title = signaltype.id;
+        signaltype = signaltype.signalType;
+        legend_strings = [legend_strings; signaltype];
+        data = currentMap(currentKeys(j));
+        time = data(:, 1);
+        time = time - initial_time;
+        time = time/1000;
+        cn0 = data(:, 2);
+        plot(time, cn0);
+    end
+    
+    postPlot(legend_strings,title);
+    
+    file_name = strcat(sessionName,"_",num2str(gnssMapKeys(i)));
+    saveGraphs(fileFolder,file_name,fig);
+    
+end
+end
